d81fbe03885583f9d6a8badb822cfe5d578f5a16
Paul Pan <panjunhong05@gmail.com>
2020-05-08 01:40:52 +0000

    Solution step 1

--- todolist/mainsite.py
+++ todolist/mainsite.py
@@ -5,5 +5,16 @@ bp = Blueprint('mainsite', __name__, url_prefix='/')
 
 @bp.route('/')
 def hello():
-    print(get_db)
-    return jsonify({'a': 12})
+    db = get_db()
+    result = db.execute('SELECT * FROM todoitem').fetchall()
+    data = []
+    for result_row in result:
+       data.append(list(result_row))
+    return jsonify(data)
+
+@bp.route('/sample', methods=['POST'])
+def sample():
+    db = get_db()
+    db.execute('INSERT INTO todoitem (title, todotype, todotext) VALUES (?, ?, ?)', ('Todo1', 'homework', 'Do homework'))
+    db.commit()
+    return jsonify('Success!')
